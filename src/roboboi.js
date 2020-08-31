cddb9ed888d8632ba1b33232cd17cd9e509b00f6
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-08-31 15:54:55 -0700

    Add student-475 (#24)
    * Change welcome channel identification method
    
    * Add Student-475 to roleCallConfig

--- src/roboboi.js
+++ src/roboboi.js
@@ -198,7 +198,7 @@ client.on("raw", packet =>
     // Grab the channel to check the message from
     const channel = client.channels.get(packet.d.channel_id);
 	//this is #welcome
-	if(channel.id == 674870421237268483) return;
+	if(packet.d.channel_id == 674870421237268483) return;
     // There's no need to emit if the message is cached, because the event will fire anyway for that|| wrong yes there is
     //if (channel.messages.has(packet.d.message_id)) return;
     // Since we have confirmed the message is not cached, let's fetch it
