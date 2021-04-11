39b2719eec7d0e1ad057fe350f5926273ea4c2cd
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-04-10 23:36:57 -0700

    Merge delay and parseTime updates from another repo

--- src/util/time/parseTime.js
+++ src/util/time/parseTime.js
@@ -10,26 +10,34 @@ const secReg = new RegExp(numberRegDef+'s','i');
 const msReg = new RegExp(numberRegDef+'ms','i');
 
 /*
- parse time inputs with flexible syntax. accepts any mix of years, weeks, days, hours, minutes, seconds, milliseconds.
+ parse time accepts inputs with flexible syntax. accepts any mix of years, weeks, days, hours, minutes, seconds, milliseconds.
  does not accept months because how many days is a month anyways? why do you need that?
  
- 1h 15m 30s 200ms
+ example input:
+ 1h15m30s200ms
  1h
  15m
  30s
  200ms
- 1h 30s
- 15m 30s
+ 1h30s
+ 15m30s
+ 4h 20m 9s
 */
-function parseTime(timeToParse)
+function parseTime(timeToParse, options)
 {
 	let timeValue = timeToParse;
+	if(options === undefined) options = {};
 	if(isNaN(timeToParse))
 	{
 		const timeString = timeToParse;
 		if(!isTimeFormat(timeString))
 		{
-			throw new TypeError('must be in the format 1d 2h 3m 4s 5ms (any segment is optional, such as `1h 1m` is valid)');
+			if(options.throwIfInvalid)
+			{
+				throw new TypeError('must be in the format 1d 2h 3m 4s 5ms (any segment is optional, such as `1h 1m` is valid)');
+			} else {
+				return NaN;
+			}
 		} else {
 			let match = null;
 			let years = 0;
